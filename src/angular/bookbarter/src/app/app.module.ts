2e905cda2028f3357ba71f77d61ccb442fc79c46
AeroX2 <james@ridey.email>
2018-03-29 23:49:58 +1100

    Added Angular Material and home,login,register page

--- src/angular/bookbarter/src/app/app.module.ts
+++ src/angular/bookbarter/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatButtonModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	BrowserAnimationsModule,
+	AppRoutingModule,
+	MatInputModule,
+	MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
